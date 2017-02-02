5aadfe6e2f631d2159258934f1e0622b72b2752c
Jaron Briggs <jaron.briggs@vivintsolar.com>
2017-02-01 20:24:42 -0700

    added redirect for route

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ let app = express();
 let port = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
-	if (req.get('host') === 'jjberrett.com') {
-		return res.redirect(301, 'http://www.jjberrett.com');
+	if (req.get('host') === 'jaronbriggs.com') {
+		return res.redirect(301, 'http://www.jaronbriggs.com');
 	}
 	next();
 });
